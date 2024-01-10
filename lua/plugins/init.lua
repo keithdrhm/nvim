b675c4f58a58441789c335fbfda9d106187c26d1
Keith Durham <keith.durham@mlb.com>
2024-01-10 09:06:14 -0800

    removed duplicate code for trouble

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -54,13 +54,7 @@ return {
   'williamboman/mason.nvim',
   'williamboman/mason-lspconfig.nvim',
   {
- "folke/trouble.nvim",
- dependencies = { "nvim-tree/nvim-web-devicons" },
- opts = {
-  -- your configuration comes here
-  -- or leave it empty to use the default settings
-  -- refer to the configuration section below
- },
-}
-
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
 }
